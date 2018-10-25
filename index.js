6c247ebc78c5466c622fca8ea2a16948f8de4ffe
Lauren Perchitti <laurenperchitti@gmail.com>
2018-10-25 12:35:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,36 +12,3 @@ function retrieveEmployeeInformation(){
   return input.value
 }
 
-function addNewLiOnClick(e){
-    if(input.value === ''){
-        alert('Add a task')
-    }
-
-    //Create li element
-    const li = document.createElement('li')
-    //add a class
-    li.className = 'collection-item'
-    //Create text node and append to li
-    li.appendChild(document.createTextNode(input.value))
-    // Create new link element
-    const link = document.createElement('a')
-    // Add class
-    link.className = 'delete-item secondary-content'
-    // Add icon html
-    link.innerHTML = '<i class="fa fa-remove"></i>'
-    //Append like to li
-    li.appendChild(link)
-
-    //Append li to ul
-    input.appendChild(li)
-
-    //Store in local storage
-    storeTaskInLocalStorage(input.value);
-
-    //clear input
-    input.value = ''
-
-
-
-e.preventDefault()
-}
